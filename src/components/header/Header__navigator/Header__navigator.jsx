70bca10cb2de6c9794a59c0c0b437c1e3faafc57
AhtsAlex <antsupov.lt@gmail.com>
2021-01-06 16:08:55 +0300

    06.01.2021

--- src/components/header/Header__navigator/Header__navigator.jsx
+++ src/components/header/Header__navigator/Header__navigator.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import classes from './Header__navigator.module.css';
+import navLogo from './logo.png';
 
 
 function Header__navigator() {
     return (<div>
+        <div  className={classes.Header__navigator}>
         
-        <p>sdfsasdsddf</p>
+            <div>
+                <ul className={classes.LeftNav}>
+                    <li><a href="#" className={classes.navLink}>Tour</a></li>
+                    <li><a href="#" className={classes.navLink}>Features</a></li>
+                    <li><a href="#" className={classes.navLink}>Pricing</a></li>
+                </ul>
+            </div>
+            <div><a href="#"><img src={navLogo} alt="New Providence"/></a></div>
+            <div><ul className={classes.RightNav}>
+                    <li><a href="#" className={classes.navLink}>Help</a></li>
+                    <li><a href="#" className={classes.navLink}>Contacts</a></li>
+                    <li><a href="#"> Get App</a></li>
+                </ul>
+            </div>
+        </div>
 
     </div>);
 
